50c6d3379a1b96a096acb27992208f0c3a45dea3
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-11-10 23:11:07 +0300

    storing dom elements

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ function Pizza(size,crust,toppings){
 	this.crust = crust;
 	this.toppings = toppings;
 };
-
+// Variables for storing HTML DOM elements
+let sizeSelector = document.getElementById("size-selector");
+let sizePrice = 0;
+let crustSelector = document.getElementById("crust-selector");
+let crustPrice = 0;
+let allToppings = document.forms[2];
+let toppingsTotal = 0;
+let orderContainer = document.getElementById("order");
+let cartContainer = document.getElementById("cart");
+let sizeName = "";
+let crustName = "";
+let toppingsName = [];
 
